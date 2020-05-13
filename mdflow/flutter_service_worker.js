e2e144a6677da46dd892d222753589ea2f833df8
Tonic Artos <tonic.artos@gmail.com>
2020-05-13 17:32:36 +0800

    Update mdflow demo to 1.0.0+10.

--- mdflow/flutter_service_worker.js
+++ mdflow/flutter_service_worker.js
@@ -3,14 +3,14 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"main.dart.js": "59b7b8e6fc4cb3886d10932ee45dd907",
+"main.dart.js": "73627905f6879a498967d41ccd3e87c9",
 "manifest.json": "00e0b69b49487ce4f9ff0c5fac8fda49",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "index.html": "926cd8008bef60a4e735ba8d18fbac9c",
 "/": "926cd8008bef60a4e735ba8d18fbac9c",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"assets/LICENSE": "af2d24a6f7a18879d60c73e4ca843167",
+"assets/LICENSE": "f630c1f30d0ac0fd7078184b1e3d7b2f",
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9"
 };
